71a946c0f06417fda7a68a596c9d80ee33f723ed
eppjonathan <epp.j.epp@googlemail.com>
2024-03-15 17:35:49 +0100

    fix: pipeline fixed

--- pages/weatherIcon.js
+++ pages/weatherIcon.js
@@ -1,4 +1,24 @@
+import React from 'react';
 import { WiDaySunny, WiRain, WiSnow, WiCloud, WiFog, WiThunderstorm, WiShowers } from 'react-icons/wi';
+
+function getWeatherIcon(weather) {
+  switch (weather) {
+    case 'Clear':
+      return <WiDaySunny />;
+    case 'Rain':
+      return <WiRain />;
+    case 'Snow':
+      return <WiSnow />;
+    case 'Clouds':
+      return <WiCloud />;
+    case 'Atmosphere':
+      return <WiFog />;
+    case 'Thunderstorm':
+      return <WiThunderstorm />;
+    case 'Drizzle':
+      return <WiShowers />;
+  }
+}
 
 export function getWeatherIcon(weather) {
   switch (weather) {
