190775c2b2494ded2aeb79cfe57a1201a7999cf1
eppjonathan <epp.j.epp@googlemail.com>
2024-03-15 16:08:25 +0100

    update: page made reactive

---
+++ pages/weatherIcon.js
@@ -0,0 +1,20 @@
+import { WiDaySunny, WiRain, WiSnow, WiCloud, WiFog, WiThunderstorm, WiShowers } from 'react-icons/wi';
+
+export function getWeatherIcon(weather) {
+  switch (weather) {
+    case 'Clear':
+      return <WiDaySunny />;
+    case 'Rain':
+      return <WiRain />;
+    case 'Snow':
+      return <WiSnow />;
+    case 'Clouds':
+      return <WiCloud />;
+    case 'Atmosphere':
+      return <WiFog />;
+    case 'Thunderstorm':
+      return <WiThunderstorm />;
+    case 'Drizzle':
+      return <WiShowers />;
+  }
+}
